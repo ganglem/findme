1e2ed5e0649275d8017032ff2a992fe50154a34f
Emilija Kastratović <emilija1705@gmail.com>
2025-05-25 21:30:09 +0200

    update timetable

--- app/timetable/page.tsx
+++ app/timetable/page.tsx
@@ -10,7 +10,7 @@ export default async function TimetablePage() {
     data: { session },
   } = await supabase.auth.getSession()
 
-  if (!session) {
+    if (!session) {
     redirect("/auth/login")
   }
 
